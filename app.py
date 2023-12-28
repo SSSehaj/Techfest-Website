2f39d27c4043f7d9f64ab056692ca7a5d7ee4442
Progpr <atharvamp@gmail.com>
2023-12-28 15:20:19 +0530

    com

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, url_for
-from cs50 import SQL
+# from cs50 import SQL
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def main():
     return render_template("main.html")
 
-@app.route("/wwz")
-def register():
-    return
+# @app.route("/wwz")
+# def register():
+#     return
 
