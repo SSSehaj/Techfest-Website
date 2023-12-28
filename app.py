854c5f98e0bdef7589ad6c2568667fd4c47c30c6
Atharva Patil <95381092+Progpr@users.noreply.github.com>
2023-12-28 08:18:07 +0000

    added cs50 for sql

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, url_for
+from cs50 import SQL
 
 app = Flask(__name__)
 
@@ -7,6 +8,6 @@ app = Flask(__name__)
 def main():
     return render_template("main.html")
 
-if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')
+# if __name__ == "__main__":
+#     app.run(debug=False,host='0.0.0.0')
 
