670542e865167d1f3dddef8a618f45e2a8620b2f
Progpr <atharvamp@gmail.com>
2023-12-28 15:20:08 +0530

    attempting sql backend

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def main():
     return render_template("main.html")
 
-# if __name__ == "__main__":
-#     app.run(debug=False,host='0.0.0.0')
+@app.route("/wwz")
+def register():
+    return
 
